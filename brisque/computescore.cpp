958a6038bb05546a70017fe59589d441ae631cf8
Anish Mittal <anish.mittal@nokia.com>
2014-04-04 12:45:02 -0700

    adding read me

--- brisque/computescore.cpp
+++ brisque/computescore.cpp
@@ -1,25 +1,33 @@
 #include "brisque.h"
     
-void computescore(){
+float computescore(char* imagename){
     
+    
+    float qualityscore;
+
     char* filename = "test.txt";
     FILE* fid = fopen(filename,"w");
     fclose(fid);
  
-     // cout<<itr<<":"<<iforg[itr]<<endl;
-     //Dont compute features for original images
-     
-      float score = 0;
+         
+    IplImage* orig = cvLoadImage(imagename);
+    vector<double> brisqueFeatures;
+    ComputeBrisqueFeature(orig, brisqueFeatures);
+    printVectortoFile(filename,brisqueFeatures,0);
+      
+    remove("output.txt");
+    remove("test_ind_scaled");
+    system("libsvm/svm-scale -r allrange test.txt >> test_ind_scaled");
+    system("libsvm/svm-predict -b 1 test_ind_scaled allmodel output.txt >>dump");
 
-  
-      IplImage* orig = cvLoadImage("testimage1.bmp");
-      vector<double> brisqueFeatures;
-      ComputeBrisqueFeature(orig, brisqueFeatures);
-      //printVector(brisqueFeatures);
-      printVectortoFile(filename,brisqueFeatures,score);
-
-     
-    return 0;
-
+    fid = fopen("output.txt","r");
+    fscanf(fid,"%f",&qualityscore);
+    fclose(fid);
+    remove("test.txt");
+    remove("dump");
+    remove("output.txt");
+    remove("test_ind_scaled");
+             
+    return qualityscore;
 }
 
